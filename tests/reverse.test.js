19442f40f36729b88e5e5d79f1e8e1d690aa2cf2
Abdul Samea <abdulsamea2@gmail.com>
2021-07-01 20:43:26 +0530

    sample test cases added

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -2,7 +2,7 @@ const app = require('../index')
 const supertest = require('supertest')
 const request = supertest(app)
 
-describe('Test cases for Posts', () => {
+describe('Test cases for reverse api', () => {
 
     it('should test that posts are fetching from get url /reverse-words', async done => {
         const response = await request.get('/reverse-words')
